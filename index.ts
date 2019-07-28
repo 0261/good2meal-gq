a1a777df677267c350a8abf537d32eb1a1f98434
0261 <bada013@naver.com>
2019-07-28 23:38:24 +0900

    엔트리포인트에서 미들웨어 추가하도록 변경

--- index.ts
+++ index.ts
@@ -1,4 +1,16 @@
 import 'reflect-metadata';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { ApplicationFactory } from './src/app';
+
 const app = ApplicationFactory.create();
+app.use([
+    morgan('dev'),
+    helmet(),
+    cors(),
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json(),
+]);
 app.start();
