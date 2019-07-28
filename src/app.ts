95fba19c80f97ba5d160562029692abac95a0ccd
김바다 / Pada <bada013@naver.com>
2019-07-28 23:26:14 +0900

    필수 미들웨어추가 (#7)
    * cors helmet body-parser morgan 추가
    
    * 필수 미들웨어 추가
    
    생성자에 추가

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
 import Express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { createSchema } from './core/createSchema';
 import { createConnection } from 'typeorm';
 import { ormConfig } from './ormconfig';
 
 class Application {
     private readonly app: Express.Application = Express();
+    constructor() {
+        this.app.use([
+            morgan('dev'),
+            helmet(),
+            cors(),
+            bodyParser.urlencoded({ extended: false }),
+            bodyParser.json(),
+        ]);
+        console.log('start');
+    }
     async start() {
         try {
             await createConnection(ormConfig);
-
             const server = new ApolloServer({
                 schema: await createSchema(),
             });
             server.applyMiddleware({ app: this.app });
-
             this.app.listen(4000, () =>
                 console.log('http://localhost:4000/graphql'),
             );
