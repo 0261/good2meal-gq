cfba9659b3fd9c1ed1c2738a87221be2409eae63
0261 <bada013@naver.com>
2019-07-28 19:40:48 +0900

    유저 테이블 추가

---
+++ src/entities/User.ts
@@ -0,0 +1,8 @@
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Base } from './Base';
+
+@Entity()
+export class User extends Base {
+    @PrimaryGeneratedColumn()
+    id!: number;
+}
