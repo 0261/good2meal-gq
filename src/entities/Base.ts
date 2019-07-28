9e5bba1f2be0d2a168ffb50587089e90574b02fd
김바다 / Pada <bada013@naver.com>
2019-07-29 00:26:20 +0900

    Pada/create table (#9)
    * 날짜 라이브러리 moment 추가
    
    * 생성자에서 this.app.use 삭제
    
    * 음식점, 키워드, 태그, 이미지, 댓글 테이블 추가
    
    * 기본 테이블로 생성날짜, 갱신날짜 추가
    
    * 음식점 테이블 추가
    
    음식점은 여러개의 키워드, 여러개의 태그, 여러개의 이미지, 여러개의 댓글을 가질 수 있음
    
    * 댓글 테이블 추가
    
    댓글은 한 음식저메 여러개가 달릴 수 있음
    
    * 이미지 테이블 추가
    
    이미지는 한개의 이미지는 한 음식점에만 포함됨
    
    * 키워드 테이블 추가
    
    키워드는 한 개는 여러개의 음식점과 매칭될 수잇음
    
    * 태그 테이블 추가
    
    하나의 태그는 여러개의 음식점과 매칭될 수 있음

--- src/entities/Base.ts
+++ src/entities/Base.ts
@@ -1,3 +1,25 @@
-import { BaseEntity } from 'typeorm';
+import { BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import moment from 'moment';
 
-export class Base extends BaseEntity {}
+export class Base extends BaseEntity {
+    @CreateDateColumn({
+        transformer: {
+            to: value => value,
+            from: value =>
+                moment(value)
+                    .locale('kr')
+                    .format('YYYY-MM-DD HH:mm:ss'),
+        },
+    })
+    createdAt!: string;
+    @UpdateDateColumn({
+        transformer: {
+            to: value => value,
+            from: value =>
+                moment(value)
+                    .locale('kr')
+                    .format('YYYY-MM-DD HH:mm:ss'),
+        },
+    })
+    updatedAt!: string;
+}
---
+++ src/entities/Comment.ts
@@ -0,0 +1,15 @@
+import { Base } from './Base';
+import { PrimaryGeneratedColumn, ManyToOne, Column, Entity } from 'typeorm';
+import { Restaurant } from './Restaurant';
+
+@Entity()
+export class Comment extends Base {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column('varchar', { comment: '댓글 내용', nullable: false })
+    content!: string;
+
+    @ManyToOne(type => Restaurant, res => res.comments)
+    restaurant!: Restaurant;
+}
