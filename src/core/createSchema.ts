c8beaf1891930b0bc6b52ca3607a285b3f90c4f2
0261 <bada013@naver.com>
2019-07-29 22:46:39 +0900

    type-graphql 컨테이너 기능 추가

--- src/core/createSchema.ts
+++ src/core/createSchema.ts
@@ -1,7 +1,8 @@
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from '../resolvers/User.resolver';
-
+import { Container } from 'typedi';
 export const createSchema = async () =>
     await buildSchema({
         resolvers: [UserResolver],
+        container: Container,
     });
