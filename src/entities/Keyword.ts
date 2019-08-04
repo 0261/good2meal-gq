1a5a25c258203658370d6100e3508d4d607c3c61
0261 <bada013@naver.com>
2019-08-04 16:34:07 +0900

    mysql관련 엔터티 제거

--- src/entities/Keyword.ts
+++ src/entities/Keyword.ts
@@ -1,15 +1,15 @@
-import { Base } from './Base';
-import { PrimaryGeneratedColumn, ManyToOne, Column, Entity } from 'typeorm';
-import { Field, ObjectType } from 'type-graphql';
+// import { Base } from './Base';
+// import { PrimaryGeneratedColumn, ManyToOne, Column, Entity } from 'typeorm';
+// import { Field, ObjectType } from 'type-graphql';
 
-@Entity()
-@ObjectType()
-export class Keyword extends Base {
-    @Field()
-    @PrimaryGeneratedColumn()
-    id!: number;
+// @Entity()
+// @ObjectType()
+// export class Keyword extends Base {
+//     @Field()
+//     @PrimaryGeneratedColumn()
+//     id!: number;
 
-    @Field()
-    @Column('varchar', { comment: '키워드 명', nullable: false })
-    name!: string;
-}
+//     @Field()
+//     @Column('varchar', { comment: '키워드 명', nullable: false })
+//     name!: string;
+// }
