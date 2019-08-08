e897a3a62e162fe3d3ded8beb112920448a12973
0261 <bada013@naver.com>
2019-08-08 13:42:27 +0900

    안쓰는 엔터티 제거

--- src/entities/User.ts
+++
@@ -1,8 +0,0 @@
-// import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-// import { Base } from './Base';
-
-// @Entity()
-// export class User extends Base {
-//     @PrimaryGeneratedColumn()
-//     id!: number;
-// }
--- src/services/database.ts
+++
@@ -1,9 +0,0 @@
-import { createConnection, Connection, ConnectionOptions } from 'typeorm';
-import { Service } from 'typedi';
-
-@Service()
-export class Database {
-    async createConnection(config: ConnectionOptions): Promise<Connection> {
-        return await createConnection(config);
-    }
-}
